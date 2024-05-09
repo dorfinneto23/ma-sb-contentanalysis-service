b92c6658ebe7f654cf7f9692e10fc035d0834357
tech dor <tech.aharoni@gmail.com>
2024-05-09 22:18:13 +0300

    adding openai_content_analysis function

--- function_app.py
+++ function_app.py
@@ -28,12 +28,47 @@ username = os.environ.get('sql_username')
 password = os.environ.get('sql_password')
 driver= '{ODBC Driver 18 for SQL Server}'
 
+#Openai function - content analysis
+def openai_content_analysis(path, caseid):
+    try:
+        logging.info(f"openai_content_analysis function strating")
+        container_name = "medicalanalysis"
+        blob_service_client = BlobServiceClient.from_connection_string(connection_string_blob)
+        container_client = blob_service_client.get_container_client(container_name)
+        blob_client = container_client.get_blob_client(path)
+        download_stream = blob_client.download_blob()
+        logging.info(f"data from the txt file is {download_stream}")
+        #preparing data for response 
+        data = { 
+            "status" : "success", 
+            "Description" : f"content analysis sucess"
+        } 
+        json_data = json.dumps(data)
+        return json_data
+    except Exception as e:
+        #preparing data for response 
+        data = { 
+            "status" : "error", 
+            "Description" : f"{str(e)}"
+        } 
+        json_data = json.dumps(data)
+        return json_data
+
 app = func.FunctionApp()
 
 @app.service_bus_queue_trigger(arg_name="azservicebus", queue_name="contentanalysis",
                                connection="medicalanalysis_SERVICEBUS") 
 def sbcontentanalysisservice(azservicebus: func.ServiceBusMessage):
     message_data = azservicebus.get_body().decode('utf-8')
-    logging.info('Received messageesds: %s', message_data)
+    logging.info(f"Received messageesds: {message_data}")
     message_data_dict = json.loads(message_data)
     caseid = message_data_dict['caseid']
+    doc_id = message_data_dict['doc_id']
+    path = message_data_dict['path']
+    url = message_data_dict['url']
+    filename = message_data_dict['filename']
+    openai_result = openai_content_analysis(path,caseid)
+    logging.info(f"Received messageesds: {openai_result}")
+
+
+
