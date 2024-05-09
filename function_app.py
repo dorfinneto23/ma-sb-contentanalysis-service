b594505e974eb6a54727bce4262d08603652581d
tech dor <tech.aharoni@gmail.com>
2024-05-09 20:59:53 +0300

    adding openai library

--- function_app.py
+++ function_app.py
@@ -6,6 +6,7 @@ import io # in order to download pdf to memory and write into memory without dis
 import json # in order to use json 
 import pyodbc #for sql connections 
 from azure.servicebus import ServiceBusClient, ServiceBusMessage # in order to use azure service bus 
+from openai import AzureOpenAI #for using openai services 
 
 
 app = func.FunctionApp()
