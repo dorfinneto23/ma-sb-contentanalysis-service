bdd0bd9bd0e8bec605242d4718b9dbafc47206cd
tech dor <tech.aharoni@gmail.com>
2024-05-09 20:39:24 +0300

    adding logs and basic libraries

--- function_app.py
+++ function_app.py
@@ -1,10 +1,19 @@
 import azure.functions as func
 import logging
+import os #in order to get parameters values from azure function app enviroment vartiable - sql password for example 
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient # in order to use azure container storage
+import io # in order to download pdf to memory and write into memory without disk permission needed 
+import json # in order to use json 
+import pyodbc #for sql connections 
+from azure.servicebus import ServiceBusClient, ServiceBusMessage # in order to use azure service bus 
+
 
 app = func.FunctionApp()
 
 @app.service_bus_queue_trigger(arg_name="azservicebus", queue_name="contentanalysis",
                                connection="medicalanalysis_SERVICEBUS") 
 def sbcontentanalysisservice(azservicebus: func.ServiceBusMessage):
-    logging.info('Python ServiceBus Queue trigger processed a messageee: %s',
-                azservicebus.get_body().decode('utf-8'))
+    message_data = azservicebus.get_body().decode('utf-8')
+    logging.info('Received messageesds: %s', message_data)
+    message_data_dict = json.loads(message_data)
+    caseid = message_data_dict['caseid']
