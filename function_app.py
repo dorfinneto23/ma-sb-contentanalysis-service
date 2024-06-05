5ab8575016609bfa95c6eebf16222dc76c5a77b4
tech dor <tech.aharoni@gmail.com>
2024-06-05 22:38:29 +0300

    time.sleep(120)

--- function_app.py
+++ function_app.py
@@ -88,14 +88,14 @@ def check_openai_available_resurces(table_name, partition_key, row_key,contentTo
         logging.info(f"get_openai_tokens_usage:currentlyTokens: {currentlyTokens},currentlyRequests: {currentlyRequests},requestsPerMinute: {requestsPerMinute},tokensPerMinute: {tokensPerMinute}")
         if ((totalTokens>tokensPerMinute*0.7) or (totalTokens == tokensPerMinute*0.7)):
             logging.info(f"waiting one minute - total tokens exceed the maximum limitation of openai,totalTokens:{totalTokens},maximum:{tokensPerMinute} ")
-            # Wait for 60 seconds
-            time.sleep(60)
+            # Wait for 120 seconds
+            time.sleep(120)
             reset_tokens_requests_usage(table_name, partition_key, row_key)
             return True
         elif ((totalRequests>requestsPerMinute*0.7) or (totalRequests == requestsPerMinute*0.7)):
             logging.info(f"waiting one minute - total Requests  exceed the maximum limitation of openai,totalRequests:{totalRequests},maximum:{requestsPerMinute} ")
-            # Wait for 60 seconds
-            time.sleep(60)
+            # Wait for 120 seconds
+            time.sleep(120)
             reset_tokens_requests_usage(table_name, partition_key, row_key)
             return True
         else:
